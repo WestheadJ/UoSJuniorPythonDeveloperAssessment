4e0fb7fa45725cd4de6f1e752fadfb63039ee80f
James Westhead <james_westhead@outlook.com>
2026-03-14 17:54:58 +0000

    added 3 end points to better match requirements

--- backend/dal/customers.py
+++ backend/dal/customers.py
@@ -1,5 +1,5 @@
 # dal/customers.py
-
+import pandas as pd
 from db.connection import get_db_conn
 
 CUSTOMER_ORDERS_QUERY = """
@@ -18,6 +18,22 @@ FROM Customers c
 LEFT JOIN Orders o ON c.customer_id = o.customer_id
 WHERE c.customer_id = ?
 """
+QUERY_ACTIVE_CUSTOMER_ORDERS = """
+SELECT 
+    c.customer_id,
+    c.first_name || ' ' || c.last_name AS name,
+    c.email,
+    c.customer_status,
+    o.order_id,
+    o.product,
+    o.quantity,
+    o.unit_price,
+    o.order_total
+FROM Customers c
+INNER JOIN Orders o
+    ON c.customer_id = o.customer_id
+WHERE c.customer_status = 'active'
+"""
 
 
 def get_customer_orders(customer_id: int):
@@ -25,3 +41,17 @@ def get_customer_orders(customer_id: int):
         cursor = conn.cursor()
         cursor.execute(CUSTOMER_ORDERS_QUERY, (customer_id,))
         return cursor.fetchall()
+
+
+def get_active_customer_orders():
+    with get_db_conn() as conn:
+        cursor = conn.cursor()
+        cursor.execute(QUERY_ACTIVE_CUSTOMER_ORDERS)
+        return cursor.fetchall()
+
+
+def get_active_customer_orders_with_pd():
+    with get_db_conn() as conn:
+        df = pd.read_sql_query(QUERY_ACTIVE_CUSTOMER_ORDERS, conn)
+
+        return df
